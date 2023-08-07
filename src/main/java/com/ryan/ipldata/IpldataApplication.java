5d113445a85d0db2692bf062749a5f1492ece664
Raushan Ryan <raushankrpnhs2000@gmail.com>
2023-08-08 00:49:09 +0530

    IplData  Project Started

---
+++ src/main/java/com/ryan/ipldata/IpldataApplication.java
@@ -0,0 +1,13 @@
+package com.ryan.ipldata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpldataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpldataApplication.class, args);
+	}
+
+}
